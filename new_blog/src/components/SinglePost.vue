da1c47198120391a8168d2e88be2bc07be5d7b09
manisha-darekar <82135338+manisha-darekar@users.noreply.github.com>
2021-09-06 18:18:21 +0530

    Add files via upload
    working on composition api

---
+++ new_blog/src/components/SinglePost.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h3>{{post.title}}</h3>
+    <h3>{{snippet}}</h3>
+</div>
+  
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+export default {
+    props:['post'],
+    setup(props){
+
+        const snippet=computed(()=>{
+
+            return props.post.body.substring(0,100)
+        })
+        return{snippet}
+
+
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ new_blog/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+   home
+   <PostList v-if="showpost" :posts="posts"/>
+   <button @click="showpost=!showpost">Toggle post</button>
+   <button @click="posts.pop()">Delete Post</button>
+
+  </div>
+</template>
+
+<script>
+import PostList from '../components/PostList.vue'
+import { ref } from '@vue/reactivity'
+
+
+export default {
+  name: 'Home',
+  components:{PostList},
+  setup(){
+    const posts=ref([{title:'Blog for HTML',body:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, porro magnam aperiam aut aspernatur suscipit neque tempora hic exercitationem odio',id:1},
+    {title:'blog for CSS',body:'css feature',id:2}])
+
+    const showpost=ref(true)
+
+    return{posts,showpost}
+  }
+
+
+   
+
+   
+
+
+    
+}
+</script>
